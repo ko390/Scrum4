5f76b0c061a5b9961447a59d1f7026b59b3d4bb3
KarlGomez <christiankarlgomez@gmail.com>
2024-05-09 23:52:20 +0200

    code refactor added BrandDAO

--- src/crud/BrandDAO.java
+++ src/crud/BrandDAO.java
@@ -1,5 +1,12 @@
 package crud;
 
+import java.sql.SQLException;
+
 public interface BrandDAO {
-    public void generateBands()
+    public void generateBands(BrandCRUD brandCRUD) throws SQLException;
+    public void viewBands(BrandCRUD brandCRUD) throws SQLException;
+    public void hireBands(BrandCRUD brandCRUD) throws SQLException;
+
+
+
 }
